e81564e44778610a2e68583f70ac3869692b1ca4
Theo Puranen Åhfeldt <34159999+theoahfeldt@users.noreply.github.com>
2022-02-17 13:48:24 +0100

    Implement demo system for combining spells

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,15 @@
 pub mod mouseclick;
+pub mod spellang;
 pub mod unit;
+
+#[cfg(test)]
+mod tests {
+    use super::spellang::*;
+    #[test]
+    fn test_spellang() {
+        let mut state = State { hp: 10 };
+        let mut dag = example_dag();
+        dag.execute(&mut state);
+        assert_eq!(state, State { hp: 9 });
+    }
+}
