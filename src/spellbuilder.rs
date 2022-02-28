05f412f8dbb247d1155daff3c4088399de6a6063
Anders Ågren Thuné <anders@aathn.org>
2022-02-28 16:27:49 +0100

    Add selection of spell cards

--- src/spellbuilder.rs
+++ src/spellbuilder.rs
@@ -16,7 +16,7 @@ impl SpellCard {
             description,
         } = self;
         let child = commands
-            .spawn_bundle(NodeBundle {
+            .spawn_bundle(ButtonBundle {
                 style: Style {
                     position: Rect {
                         left: Val::Px(position.x),
@@ -32,6 +32,7 @@ impl SpellCard {
                 color: Color::rgb(0.6, 0.7, 0.2).into(),
                 ..Default::default()
             })
+            .insert(SpellCardTag)
             .with_children(|parent| {
                 // Title
                 parent.spawn_bundle(TextBundle {
@@ -65,11 +66,55 @@ impl SpellCard {
     }
 }
 
+#[derive(Component)]
+struct Selected(Vec2);
+
+#[derive(Component)]
+struct SpellCardTag;
+
+fn select_card_system(
+    mut commands: Commands,
+    windows: Res<Windows>,
+    interaction_query: Query<
+        (Entity, &Style, &Interaction),
+        (Changed<Interaction>, With<SpellCardTag>),
+    >,
+) {
+    let window = windows.get_primary().unwrap();
+    if let Some(position) = window.cursor_position() {
+        for (card, style, interaction) in interaction_query.iter() {
+            if let Interaction::Clicked = interaction {
+                if let (Val::Px(x), Val::Px(y)) = (style.position.left, style.position.bottom) {
+                    commands
+                        .entity(card)
+                        .insert(Selected(Vec2::new(x - position.x, y - position.y)));
+                }
+            } else {
+                commands.entity(card).remove::<Selected>();
+            }
+        }
+    }
+}
+
+fn move_card_system(
+    mut cursor_evr: EventReader<CursorMoved>,
+    mut card_query: Query<(&mut Style, &Selected), With<SpellCardTag>>,
+) {
+    for (mut style, Selected(pos)) in card_query.iter_mut() {
+        for ev in cursor_evr.iter() {
+            style.position.left = Val::Px(ev.position.x + pos.x);
+            style.position.bottom = Val::Px(ev.position.y + pos.y)
+        }
+    }
+}
+
 pub struct SpellBuilderPlugin;
 
 impl Plugin for SpellBuilderPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(setup);
+        app.add_startup_system(setup)
+            .add_system(select_card_system)
+            .add_system(move_card_system);
     }
 }
 
